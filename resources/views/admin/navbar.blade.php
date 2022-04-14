f24643d7f42e2f6803e2fa2951b01270537df40a
Bhavya4951 <bhavyakumarpanchotiya2014@gmail.com>
2022-04-14 23:46:38 +0530

    Login System

--- resources/views/admin/navbar.blade.php
+++ resources/views/admin/navbar.blade.php
@@ -91,24 +91,11 @@
                 </li>
                 <li>
                     <div class="dropdown drp-user">
-                        <a href="javascript:" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="icon feather icon-settings"></i>
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right profile-notification">
-                            <div class="pro-head">
-                                <img src="assets/images/user/avatar-1.jpg" class="img-radius" alt="User-Profile-Image">
-                                <span>John Doe</span>
-                                <a href="auth-signin.html" class="dud-logout" title="Logout">
-                                    <i class="feather icon-log-out"></i>
-                                </a>
-                            </div>
-                            <ul class="pro-body">
-                                <li><a href="javascript:" class="dropdown-item"><i class="feather icon-settings"></i> Settings</a></li>
-                                <li><a href="javascript:" class="dropdown-item"><i class="feather icon-user"></i> Profile</a></li>
-                                <li><a href="message.html" class="dropdown-item"><i class="feather icon-mail"></i> My Messages</a></li>
-                                <li><a href="auth-signin.html" class="dropdown-item"><i class="feather icon-lock"></i> Lock Screen</a></li>
-                            </ul>
-                        </div>
+                        
+                        <x-app-layout>
+   
+                       </x-app-layout>
+                        
                     </div>
                 </li>
             </ul>
---
+++ resources/views/user/usermain.blade.php
@@ -0,0 +1,3 @@
+<h1> hello Im in user side login</h1> <x-app-layout>
+   
+   </x-app-layout>
